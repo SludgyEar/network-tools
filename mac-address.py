d51c2fa9851ddb71d7fd5665e950641e2dd1028c
SludgyEar <l21140258@queretaro.tecnm.mx>
2025-01-05 18:41:35 -0600

    Escaneo y Obtención de Mac Address

--- mac-address.py
+++ mac-address.py
@@ -1,6 +1,5 @@
 import subprocess
 import argparse
-
 parser = argparse.ArgumentParser(description='Realiza un escaneo dada una red.')
 parser.add_argument('network', metavar='NETWORK', type=str, help='La red a escanear')
 
@@ -15,16 +14,37 @@ except ImportError:
         print("Error al instalar la librería python-nmap")
 
 def escaneo_red(network):
-    nmap = nmap.PortScanner()
-    result = nmap.scan(hosts=network, arguments='-sL')
-    return result
+    nm = nmap.PortScanner()
+    try:
+        total_hosts = nm.scan(hosts=network, arguments='-sL')
+        upDevices = {}
+        for host in total_hosts['scan']:
+            if total_hosts['scan'][host]['status']['state'] != 'down':
+                upDevices[host] = total_hosts['scan'][host]
+            else:
+                continue
+        return upDevices
+    except Exception as e:
+        print(f"Error al escanear la red. Inténtelo de nuevo. {e}")
+        return{}
 
-def show_devices(result):
-    for host in result['scan']:
-        print(f"Host: {host}, Estado {result['scan'][host]['status']['state']}")
-
+def obtener_mac_address(upDevices):
+    mac_address = {}
+    for ip in upDevices:
+        if 'mac' in upDevices[ip]['addresses']:
+            mac_address[ip] = upDevices[ip]['addresses']['mac']
+        else:
+            mac_address[ip] = "No se pudo obtener la dirección MAC..."
+    return mac_address
 
 if __name__ == '__main__':
     args = parser.parse_args()
     network = args.network
-    show_devices(escaneo_red(network))
+    upDevices = escaneo_red(network)
+
+    if upDevices:
+        mac_address = obtener_mac_address(upDevices)
+        for ip, mac in mac_address.items():
+            print(f"IP: {ip} - MAC: {mac}")
+    else:
+        print("No se encontraron dispositivos en la red...")
